5f43eaedc7911902e8dcd9a3d21991cb86b2a536
MartaGago <martagago92@hotmail.com>
2021-10-21 12:23:59 +0200

    Sustituir throws por try/catch

--- src/Ejemplo1.java
+++ src/Ejemplo1.java
@@ -1,7 +1,10 @@
+import java.io.File;
+
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.ParserConfigurationException;
 import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
@@ -11,20 +14,69 @@ import org.w3c.dom.Document;
 
 public class Ejemplo1 {
 
-	public static void main(String[] args) throws ParserConfigurationException, TransformerException {
+	private static Document documento;
 
-		DocumentBuilderFactory factoria = DocumentBuilderFactory.newInstance();
-		DocumentBuilder db = factoria.newDocumentBuilder();
-		Document documento = db.newDocument();
-		
-		
-		TransformerFactory tf = TransformerFactory.newInstance();
-		Transformer transformer = tf.newTransformer();
-		DOMSource domSource = new DOMSource(documento);
-		StreamResult sr = new StreamResult();
-		transformer.transform(domSource, sr);
-		
+	public static void main(String[] args) /*throws ParserConfigurationException, TransformerException*/ {
 
+		
+		try {
+			DocumentBuilderFactory factoria = DocumentBuilderFactory.newInstance();
+			DocumentBuilder documentBuilder = factoria.newDocumentBuilder();
+			documento = documentBuilder.newDocument();
+			
+			
+			TransformerFactory transformFactory = TransformerFactory.newInstance();
+			Transformer transformer = transformFactory.newTransformer();
+			DOMSource dom = new DOMSource(documento);
+			StreamResult sr = new StreamResult(new File(""));
+			transformer.transform(dom, sr);
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		} catch (TransformerConfigurationException e) {
+			e.printStackTrace();
+		} catch (TransformerException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//		DocumentBuilderFactory factoria = DocumentBuilderFactory.newInstance();
+//		DocumentBuilder db = factoria.newDocumentBuilder();
+//		Document documento = db.newDocument();
+//		
+//		
+//		TransformerFactory tf = TransformerFactory.newInstance();
+//		Transformer transformer = tf.newTransformer();
+//		DOMSource domSource = new DOMSource(documento);
+//		StreamResult sr = new StreamResult(new File(""));
+//		transformer.transform(domSource, sr);
